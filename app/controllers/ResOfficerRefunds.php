5c9c0885fad696427c30fa2be686e3d96a1cb0ee
HarshanaC <harshanawalpita@gmail.com>
2020-12-31 15:39:10 +0530

    refrund email

--- app/controllers/ResOfficerRefunds.php
+++ app/controllers/ResOfficerRefunds.php
@@ -1,5 +1,8 @@
-<?php 
-	
+<?php
+
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
 	class ResOfficerRefunds extends Controller {
 
 		public function __construct() {
@@ -37,27 +40,72 @@
             'ticketIdError'=>''
 			];
 
+			$emails=$this->resofficerRefundModel->getPassengerEmail($data['ticketId']);
+            $dates=$this->resofficerRefundModel->checkDate($data['ticketId']);
+            $dates->seat_date;
+            $dates->cancelled_date;
+
 			if(empty($data['ticketId'])){
                 $data['ticketIdError']='Please Enter the ticket ID.';
-                }else{
-                    $dates=$this->resofficerRefundModel->checkDate($data['ticketId']);
-                    $dates->seat_date;
-                    $dates->cancelled_date;
-
-                    if($dates->seat_date!=$dates->cancelled_date){
-                    	$data['ticketIdError']='This Train is not cancelled.';
-                    }
-                    else{
-                        if ($this->resofficerRefundModel->refund($data)){				
-					        header("Location: " . URLROOT . "/ResOfficers/index");								
-			            }else{
-				            die("Something Going Wrong");
-			            }
-                    }                                         
                 }
-            
+            elseif($dates->seat_date!=$dates->cancelled_date){
+                $data['ticketIdError']='This Train is not cancelled.';
+                }
+            if(empty($data['ticketIdError'])){
+
+                if ($this->resofficerRefundModel->refund($data)){
+                    $this->informPassengerOftheRefund($emails->email);				
+				    header("Location: " . URLROOT . "/ResOfficers/index");								
+			    }else{
+				    die("Something Going Wrong");
+			    }
+            }                                                                     
 		}
 	
 		$this->view('resofficers/refunds/refund', $data); 
-	    }			
+	    }
+
+	    public function informPassengerOftheRefund($email)
+        {   
+            require APPROOT . '/libraries/PHPMailer/src/Exception.php';
+            require APPROOT . '/libraries/PHPMailer/src/PHPMailer.php';
+            require APPROOT . '/libraries/PHPMailer/src/SMTP.php';
+
+            $mail = new PHPMailer(true);
+
+            try {
+                //Server settings   
+                $mail->isSMTP();                                            // Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+                $mail->Username   = 'raillankaproject@gmail.com';                     // SMTP username
+                $mail->Password   = 'Raillanka@2';                               // SMTP password
+                $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+                //Recipients
+                $mail->setFrom('raillankaproject@gmail.com', 'RailLanka');
+                $mail->addAddress($email);     // Add a recipient
+                        // Name is optional
+                $mail->addReplyTo('no-reply@example.com', 'Information', 'No reply');
+            
+                // Content
+                $mail->isHTML(true); 
+ 
+                // Set email format to HTML
+                $mail->Subject = 'Ticket Refund';
+                $mail->Body    = "<h1>You have successfully refunded your ticket.</h1><p>Your railway ticket has refunded.
+                You can visit the railway station with your NIC and collect your money.</p>";
+                $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+                $mail->send();
+                // $msg = 'Reset password link has been sent to your email';
+                return;
+
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        
+			exit();
+        }			
 	}
