dd0f1ee632b3f4e625c1a9bc48310eb033497aa7
Navodyasankalani <sankalaninavodya@gmail.com>
2021-03-29 19:30:08 +0530

    add refund report for view

--- app/views/admins/reports/add_refund_details.php
+++ app/views/admins/reports/add_refund_details.php
@@ -6,61 +6,68 @@
     <?php
        require APPROOT . '/views/includes/admin_navigation.php';
     ?>
-
+<!-- <?php var_dump($data["trains"]) ?> -->
 <div class="body-section">
 
-            <!--<div class="content-row">-->
+            <div class="content-row">
                 <ul class="breadcrumb">
                     <li><a href="<?php echo URLROOT; ?>/admins/index">Home</a></li>
                     <li><u>Reports</u></li>
                     <li><a href="<?php echo URLROOT; ?>/adminReports/addRefundDetails">Add Refund Report Details</a></li>
                 </ul>
-            
+            </div>
 
             <div class="content-flexrow">
                 <div class="container">
                     <div class="text">Add Refund Report Details</div>
-                    <form action="<?php echo URLROOT; ?>/adminReports/refundReport" method="POST">
+                    <form action="<?php echo URLROOT; ?>/adminReports/addRefundDetails" method="POST">
 
-
-                     <div class="form-row">
+<!-- <div class="form-row"></div>
+ -->                     <div class="form-row">
                             <div class="input-data">
-                                <label for="dest">Select Train</label>
-                                <select name="dest" id="dest">
-                                    <option value="t1">All Trains</option>
-                                    <option value="t1">Galuu Kumaree</option>
-                                    <option value="t2">Udarata Manike</option>
-                                    <option value="t2">Badulu Dewi</option>
-                                    <option value="t2">Ruhunu Kumaree</option>
+                                <label for="name">Select Train</label>
+                               
+                                <select name = "trainName">
+
+                                <?php foreach ($data["trains"] as $train): ?>
+                                <option value= "<?php echo $train->trainId; ?>"> <?php echo $train->name; ?></option>
+                                <?php endforeach ;?>
+
+                                     <!-- <?php echo $data['train']->name;?> -->
+
+                                 <span class="invalidFeedback">
+                                    <?php echo $data['idError']; ?>
+                                </span>    
                                 </select>
+
+
+                                
                             </div>
                     </div>
                     <br>
                      <div class="form-row">
                         <!--<label>Select Duration</label>-->
                             <div class="input-data">
-                                <label for="date">From</label>
-                                <input type="date" id="date" >
+                                <label for="from_date">From</label>
+                                <input type="date" name="fromDate" id="from_date" max= "<?php echo date("Y-m-d");?>" >
+
+                                <span class="invalidFeedback">
+                                    <?php echo $data['fromError']; ?>
+                                </span>
+
                             </div>
                             <div class="input-data">
-                                <label for="date">To</label>
-                                <input type="date" id="date" >
+                                <label for="to_date">To</label>
+                                <input type="date" name="toDate" id="to_date" max= "<?php echo date("Y-m-d");?>" >
+
+                                <span class="invalidFeedback">
+                                    <?php echo $data['toError']; ?>
+                                </span>
+
                             </div>
                         
                     </div>
                     <br>
-                     <!--<div class="form-row">
-                            <div class="input-data">
-                                <label for="dest">Order By</label>
-                                <select name="dest" id="dest">
-                                    <option value="1">1</option>
-                                    <option value="2">2</opttion>
-                                </select>
-                            </div>
-                    </div>
-                    <br>-->
-    
-
 
 
 
@@ -69,7 +76,7 @@
                                 <input onclick="location.href='<?php echo URLROOT; ?>/adminReports/refundReport' "   type="submit" class="blue-btn" value="Create Report">
                             </div> 
                             <div class="input-data">
-                                <input onclick="history.go(-1);" type="submit" class="red-btn" value="Back"  >
+                                <input onclick="history.go(-1);" type="button" class="red-btn" value="Back"  >
                             </div>   
                             
                         </div>
