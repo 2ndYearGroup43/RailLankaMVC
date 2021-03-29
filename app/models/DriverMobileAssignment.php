2e1e4a9ee870f80f79d33182755ee502662bd875
Harshana Hasanka <hhasankae@gmail.com>
2021-03-29 19:00:39 +0530

    added date consideration mobile assignments

--- app/models/DriverMobileAssignment.php
+++ app/models/DriverMobileAssignment.php
@@ -9,15 +9,32 @@ class DriverMobileAssignment
         $this->db=new Database;
     }
 
-    public function checkCurrentAssignments($driverId){
+    public function checkCurrentAssignments($driverId, $date){
         $this->db->query("SELECT src.name AS src_station_name, dest.name AS dest_station_name ,t.*, da.*, j.* FROM driver_assignment da
 			INNER JOIN journey j ON j.journeyId=da.journeyId  
             INNER JOIN train t ON j.trainId=t.trainId
             INNER JOIN station src ON t.src_station=src.stationID
             INNER JOIN station dest ON t.dest_station=dest.stationID
-            WHERE da.driverId=:driverId AND j.journey_status <> 'Ended'
+            WHERE da.driverId=:driverId AND j.journey_status <> 'Ended' AND j.date=:date 
         ");
         $this->db->bind(':driverId', $driverId);
+        $this->db->bind(":date", $date);
+        $row=$this->db->single();
+        return $row;
+    }
+
+//considering night mail trains
+    public function checkCurrentOverNightAssignments($driverId, $date, $prevDate){
+        $this->db->query("SELECT src.name AS src_station_name, dest.name AS dest_station_name ,t.*, da.*, j.* FROM driver_assignment da
+			INNER JOIN journey j ON j.journeyId=da.journeyId  
+            INNER JOIN train t ON j.trainId=t.trainId
+            INNER JOIN station src ON t.src_station=src.stationID
+            INNER JOIN station dest ON t.dest_station=dest.stationID
+            WHERE da.driverId=:driverId AND j.journey_status <> 'Ended' AND (j.date=:date OR j.date =:prevDate)
+        ");
+        $this->db->bind(':driverId', $driverId);
+        $this->db->bind(":date", $date);
+        $this->db->bind(":prevDate", $prevDate);
         $row=$this->db->single();
         return $row;
     }
@@ -31,6 +48,16 @@ class DriverMobileAssignment
         $this->db->bind(":driverId", $driverId);
         $results=$this->db->resultSet();
         return $results;
+    }
+
+    public function getAssignedTrain($driverId){
+        $this->db->query("SELECT t.*, da.*, j.* FROM driver_assignment da
+			INNER JOIN journey j ON j.journeyId=da.journeyId  
+            INNER JOIN train t ON j.trainId=t.trainId
+            WHERE da.driverId=:driverId AND j.journey_status <> 'Ended'");
+        $this->db->bind(':driverId', $driverId);
+        $row=$this->db->single();
+        return $row;
     }
 
 }
